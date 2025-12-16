5460b0f27903487bb8c8d70e3ec78736d0ee4765
kingsley <kingsleykay26@gmail.com>
2025-12-16 20:40:08 +0100

    created data.js; an array of flashcard questions and answers

---
+++ scripts/data.js
@@ -0,0 +1,49 @@
+export const data = [
+  {
+    id: 1,
+    question: "What is the difference between var, let and const?",
+    answer:
+      "In JavaScirpt, var is function-scoped and can be re-declared; let and const are block-scoped, with let allowing re-assignment and const preventing it. However, const objects can have their contents modified.",
+  },
+  {
+    id: 2,
+    question: "Difference between <div> and <section>?",
+    answer:
+      "<div> is non-semantic and purely for grouping. <section> represents a meaningful section of content with a theme.",
+  },
+  {
+    id: 3,
+    question: "What is the difference between id and class?",
+    answer:
+      "id is unique per page and used once; class can be reused on multiple elements.",
+  },
+  {
+    id: 4,
+    question: "What is an object in JavaScript?",
+    answer:
+      "A collection of key–value pairs used to store structured data and behavior.",
+  },
+  {
+    id: 5,
+    question: "Difference between array and object?",
+    answer:
+      "Arrays store ordered data (indexed), objects store named properties.",
+  },
+  {
+    id: 6,
+    question: "Why is semantic HTML important?",
+    answer: "Improves accessibility, SEO, and code readability.",
+  },
+  {
+    id: 7,
+    question: "What is the purpose of <label> in forms?",
+    answer: "Improves accessibility and links text to form inputs.",
+  },
+  {
+    id: 8,
+    question: "What is event delegation?",
+    answer: "Handling events from a parent element.",
+  },
+];
+
+console.log(typeof data);
