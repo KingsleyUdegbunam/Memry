3bf36d7cd8ff0a50205bacd95ec733f780ace3d1
kingsley <kingsleykay26@gmail.com>
2025-12-18 14:05:36 +0100

    fixed typo(s)

--- scripts/data.js
+++ scripts/data.js
@@ -3,7 +3,7 @@ export const data = [
     id: 1,
     question: "What is the difference between var, let and const?",
     answer:
-      "In JavaScirpt, var is function-scoped and can be re-declared; let and const are block-scoped, with let allowing re-assignment and const preventing it. However, const objects can have their contents modified.",
+      "In JavaScript, var is function-scoped and can be re-declared; let and const are block-scoped, with let allowing re-assignment and const preventing it. However, const objects can have their contents modified.",
   },
   {
     id: 2,
@@ -37,7 +37,7 @@ export const data = [
   {
     id: 7,
     question: "What is the purpose of <label> in forms?",
-    answer: "Improves accessibility and links text to form inputs.",
+    answer: "To improve accessibility and links text to form inputs.",
   },
   {
     id: 8,
